d59fb2763451430cb0e47ca1a1f6cb4346cf6d85
Derrick Burns <derrick@tidepool.org>
2020-02-27 19:57:26 -0800

    Altered env names

--- server/config.go
+++ server/config.go
@@ -24,28 +24,28 @@ type Config struct {
 
 // FromEnv populates Config from the environment
 func (c *Config) FromEnv() {
-	c.Scheme = os.Getenv("Scheme")
-	c.Hosts = os.Getenv("Addresses")
-	c.Username = os.Getenv("Username")
-	c.Password = os.Getenv("Password")
-	c.Database = os.Getenv("Database")
-	c.OptParams = os.Getenv("OptParams")
-	c.TLS = os.Getenv("Tls") == "true"
-	timeoutStr := os.Getenv("Timeout")
+	c.Scheme = os.Getenv("MONGO_SCHEME")
+	c.Hosts = os.Getenv("MONGO_ADDRESSES")
+	c.Username = os.Getenv("MONGO_USERNAME")
+	c.Password = os.Getenv("MONGO_PASSWORD")
+	c.Database = os.Getenv("MONGO_DATABASE")
+	c.OptParams = os.Getenv("MONGO_OPT_PARAMS")
+	c.TLS = os.Getenv("MONGO_TLS") == "true"
+	timeoutStr := os.Getenv("MONGOPROXY_TIMEOUT")
 	timeout, err := strconv.Atoi(timeoutStr)
 	if (timeoutStr == "") || (err != nil) {
 		c.Timeout = time.Duration(20 * time.Second)
 	} else {
 		c.Timeout = time.Duration(timeout) * time.Second
 	}
-	portStr := os.Getenv("Port")
+	portStr := os.Getenv("MONGOPROXY_PORT")
 	port, err := strconv.Atoi(portStr)
 	if (portStr == "") || (err != nil) {
 		c.Port = 27017
 	} else {
 		c.Port = port
 	}
-	c.ReadOnly = os.Getenv("Readonly") == "true"
+	c.ReadOnly = os.Getenv("MONGOPROXY_READONLY") == "true"
 }
 
 // AsConnectionString constructs a MongoDB connection string from a Config
